992ff87ee613ca0bfe0671515a1437d0323c589e
David Aaron <dsaaron123@gmail.com>
2017-01-18 00:50:09 +1100

    Updated hello world printing to actually print hello world (and more)

--- 01_helloWorld/main.go
+++ 01_helloWorld/main.go
@@ -3,6 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("second print")
-	fmt.Println("test", "third", "print")
+	fmt.Println("Hello udemy training course and world!")
+	fmt.Println("How does this function work?")
+	fmt.Println("It seems to unpack each argument passed in and then " +
+		"combine them into a byte array before passing to stdout")
+	fmt.Println("very", "interesting", "indeed")
+	fmt.Println("I dont know enough about go yet to understand whats " +
+		"going on when looking at the function calls in print.go")
+	fmt.Println("Im sure after this course it will all make sense! I " +
+		"really like the fact this reminds me of an updated C " +
+		"language already :)")
 }
