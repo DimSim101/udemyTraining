f325416d7fdfb22ec9de60be2037eff169f1418f
David Aaron <dsaaron123@gmail.com>
2017-01-22 18:02:11 +1100

    Added runes test case along with a bunch of prints for different stuff

--- runes/main.go
+++ runes/main.go
@@ -5,7 +5,21 @@ import (
 )
 
 func main() {
+
+	fmt.Println()
 	// since a rune is an alias for an int32, we must convert characters
 	// to strings in order to get their value as a string not an int.
-	fmt.Printf("testing runes" + string('a'))
+	fmt.Println(`testing runes on rune 'a' without string() conversion:`,
+		'a')
+	fmt.Println(`testing runes on rune 'a' with conversion:`, string('a'))
+
+	fmt.Println()
+
+	b := "bbb"
+	fmt.Println("Multicharacter string created:", b)
+	runeB := []rune(b)
+	fmt.Println(`Converted into slice of rune's`, runeB)
+
+	s := string(runeB)
+	fmt.Println("Converted back into string with contents:", s)
 }
