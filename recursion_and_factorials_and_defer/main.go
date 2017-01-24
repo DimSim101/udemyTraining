042ce867c636502573aa130ddf3339f3544c8372
David Aaron <dsaaron123@gmail.com>
2017-01-24 15:18:29 +1100

    Added defer stuff, comments and renamed folder to show defer is included here. Also added a sample comment explaing where defer may be used.

---
+++ recursion_and_factorials_and_defer/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	//"time"
+	"time"
+)
+
+func factorial (x int) int {
+	total := 1
+	for x > 0 {
+		total *= x
+		x--
+	}
+	return total
+}
+
+func recursiveFactorial (x int) int {
+	if x == 0 {
+		return 1
+	}
+	return x * recursiveFactorial(x-1)
+}
+
+func main() {
+	for i := 0; i < 10; i++ {
+		fmt.Printf("Factorial of %d: %d = %d Recursive Factorial \n",
+			i, factorial(i), recursiveFactorial(i))
+
+	}
+
+	// Defer the recursive calculation until after the normal one has finished
+	defer fmt.Println("RECURSIVE FACTORIAL OF 42 = ", recursiveFactorial(42))
+	// Simulate some kind of wait condition here
+	// For example, we might defer some function so that it will always happen
+	// after the result of some async function has returned.
+	// This would be useful for multithreading / concurrency stuff, as well as
+	// doing stuff in the background (same thing) like a web request.
+	time.Sleep(5)
+	fmt.Println("FACTORIAL OF 42 = ", factorial(42))
+}
