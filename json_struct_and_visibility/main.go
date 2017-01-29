0d063e5577609ab1387cf347eee5829c33c68395
David Aaron <dsaaron123@gmail.com>
2017-01-30 03:38:24 +1100

    Added way more to the example including json encoding/decoding and some more reader/writer stuff

--- json_struct_and_visibility/main.go
+++ json_struct_and_visibility/main.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"encoding/json"
 	"log"
+	"os"
+	"strings"
 )
 
 type animal struct {
 	// Here I have added some tags to show their usage in an example
 	// and also make use of them by cleaning up some variable names so
 	// the printed example json reads nicely.
+	// These tags apply to both changing the value when marshaling (to get
+	// some data in json form) as well as when unmarshalling json data (as
+	// now the data is misslabelled and needs to be identified).
+	// For example, for Category below, the json data is "Type":"BLAH"
+	// Hence, the program needs some way to trace from Type -> Category.
 	Category string `json:"Type"`
 	NumLegs int `json:"Number of legs"`
 	Name string
@@ -36,16 +43,88 @@ func newAnimal (category string, numLegs int, name string, colour string) animal
 }
 
 func main() {
+
+	fmt.Println("Creating animal data to turn into json via marshal...")
+	fmt.Println()
+
 	a := newAnimal("Canine", 4, "Chichi", "Mixed")
 	a2 := newAnimal("Canine", 4, "Bowie", "Black")
 
 	l := []animal{a, a2}
 
+	fmt.Println("Printing data of animals to show all visible and" +
+		"non visible data")
+	fmt.Println(l)
+	fmt.Println()
 	data, error := json.Marshal(l)
 
 	if error != nil {
 		log.Fatal(error)
 	}
 
-	fmt.Println(string(data))
+
+	fmt.Println("Json data:", string(data))
+	fmt.Println()
+	fmt.Println("Unmarshalling json data...")
+
+	var animalList []animal
+	var jsonAnimal animal
+	var jsonAnimal2 animal
+
+	fmt.Println(animalList)
+	error = json.Unmarshal(data, &animalList)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+	jsonAnimal = animalList[0]
+	jsonAnimal2 = animalList[1]
+
+	fmt.Println(animalList)
+	fmt.Println("json animals:", jsonAnimal, "-", jsonAnimal2)
+	fmt.Println()
+	// Here we can see the externally visible (to json package) variables
+	// have values and the rest (colour and VisibleButExcludedColour)
+	// still have their nil value (as they were assigned their nil value
+	// upon creation of the animal object).
+
+	fmt.Println("category:", jsonAnimal.Category, "-",
+		"num legs:", jsonAnimal.NumLegs, "-",
+		"name:", jsonAnimal.Name, "-",
+		"colour", jsonAnimal.colour, "-",
+		"Colour", jsonAnimal.Colour, "-",
+		"VisibleButExcludedColour", jsonAnimal.VisibleButExcludedColour, "-")
+
+	fmt.Println()
+	fmt.Println("Ending marshalling/unmarshalling example...")
+	fmt.Println()
+	fmt.Println("Starting encoding/decoding example...")
+	fmt.Println()
+
+	chichi := newAnimal("Canine", 4, "Chichi", "Mixed")
+	fmt.Println(chichi) // show all content inside animal
+
+	// json.NewEncoder(os.Stdout) returns a variable of type *Encoder
+	// which writes to Stdout (console - prints to screen essentially).
+	// As the function Encode() in the json package has a receiver of
+	// type *Encoder (its a method of the type *Encoder), we can then
+	// call .Encode on the value returned and provide some data to encode.
+	error = json.NewEncoder(os.Stdout).Encode(chichi)
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	data, error = json.Marshal(chichi)
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	var chichi2 animal
+	fmt.Println("Chichi2 after init:", chichi2)
+	fmt.Println("Json string data:", string(data))
+
+	reader := strings.NewReader(string(data))
+	error = json.NewDecoder(reader).Decode(&chichi2)
+	fmt.Println("Chichi2 after decode:", chichi2)
 }
