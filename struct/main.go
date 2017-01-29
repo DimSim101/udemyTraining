1572508aba38c7915c1291779e808ec8e4ede2de
David Aaron <dsaaron123@gmail.com>
2017-01-29 23:43:16 +1100

    Extended example to print addresses of struct pointer and the variables inside the struct. Here we can see the address of the struct pointer is purely the address of the start of the struct storage in memory, which is just the address of the first variable inside the struct.

--- struct/main.go
+++ struct/main.go
@@ -22,8 +22,23 @@ func main() {
 	// initialize struct using var default nil value -- same as below
 	var p2 person
 	fmt.Println(p2)
+	fmt.Println(p2.age)
 
 	// initialize struct to empty value -- same as above
 	p3 := person{}
 	fmt.Println(p3)
+	fmt.Println(p3.age)
+	fmt.Println()
+
+	// Pointer to p1 is just a pointer to the first variable in the struct
+	// Presumably this is just the start of the memory chunk in which
+	// all the struct variables are placed
+	fmt.Printf("%p \n", &p1)
+	fmt.Printf("%p - %p - %p \n\n", &p1.first, &p1.last, &p1.age)
+
+	fmt.Printf("%p \n", &p2)
+	fmt.Printf("%p - %p - %p \n\n", &p2.first, &p2.last, &p2.age)
+
+	fmt.Printf("%p \n", &p3)
+	fmt.Printf("%p - %p - %p \n", &p3.first, &p3.last, &p3.age)
 }
