ba5df949ebd67e4dffd3adf79b325f6fe288a5f1
David Aaron <dsaaron123@gmail.com>
2017-01-30 19:13:55 +1100

    Added example for empty interface and showed examples using both a single empty interface as well as functions taking a variadic argument of type empty interface. Some interesting things you can do here. Also showed how the ... syntax works for expanding variadic arguments with these interfaces and put comments explaining why a function that takes a single argument that is variadic cannot then have multiple arguments in its call as well as a variadic arg. It must be either a number of arguments or a single variadic arg. Interesting

--- interfaces/main.go
+++ interfaces/main.go
@@ -5,6 +5,11 @@ import (
 	"math"
 )
 
+// This is an empty interface and everything implements it (as it has no methods).
+// Thus, we can use empty interfaces to do interesting things like store variables
+// of any type.
+type empty interface {}
+
 type shape interface {
 	area() float64
 	width() float64
@@ -41,10 +46,47 @@ func info(z shape) {
 	fmt.Println(z.width())
 }
 
+// Here we can see with an empty interface parameter the function can take in
+// values of any type.
+func emptyInterface(a interface{}) {
+	fmt.Printf("%T = %v \n", a, a)
+}
+
+func emptyInterfaceVariadic(a ...interface{}) {
+	fmt.Printf("%T = %v \n", a, a)
+}
+
 func main() {
 	s := square{4}
 	info(s)
 
 	c := circle{3}
 	info(c)
+
+	// This is a slice of empty interfaces and thus the types inside can be
+	// anything.
+	emptySlice := []empty{s, c, "Testing", 420, 'a', []int{0,1,2,3}}
+	fmt.Println(emptySlice)
+
+	ints := []int{0,1,2,3}
+	emptyInterface(s)
+	emptyInterface(c)
+	emptyInterface(emptySlice)
+	emptyInterface(ints)
+
+	interfaces := []interface{}{"Blah", "Blah2", 42, []int{9,8,7}}
+	fmt.Println(interfaces)
+	// Here, the slice of interface{} is the argument and thus only
+	// takes up one entry in the printed interfaces variable.
+	emptyInterfaceVariadic(s, c, emptySlice, ints, interfaces)
+
+	// The blah... syntax cannot be used with other arguments because it
+	// expands to an unlimited number of args. We cannot do stuff like
+	// emptyInterfaceVariadic(s, c, interfaces...)
+	// This causes each value inside the array of inteface{} to get broken
+	// into individual arguments. Thus, each element inside the array is
+	// a separate element within the array of interfaces that gets printed
+	// (rather than being chunked together as above).
+	emptyInterfaceVariadic(interfaces...)
+
 }
