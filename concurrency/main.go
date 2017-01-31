185631e8ea02f9cc83c606e17c63b1fd961cbb21
David Aaron <dsaaron123@gmail.com>
2017-01-31 15:43:09 +1100

    Added example showing atomic increment using the atomic package. Also showed how this results in not needing a mutex as there is no longer a 'critical region' since the only instruction is atomic. Added in some commented out prints to show the race condition would return if using non-atomic instructions as well, and thus would require a mutex.

--- concurrency/main.go
+++ concurrency/main.go
@@ -6,11 +6,13 @@ import (
 	"time"
 	"math/rand"
 	"runtime"
+	"sync/atomic"
 )
 
 var semaphore sync.WaitGroup
 var mutex sync.Mutex
 var counter int
+var atomicCounter int64
 
 // Not sure if this is still needed? May as well put it in to be 100% were using
 // all cpu's available.
@@ -48,6 +50,13 @@ func main () {
 	go newInc()
 	semaphore.Wait()
 	fmt.Println("Final counter value is:", counter)
+	fmt.Println()
+
+	semaphore.Add(2)
+	go atomicInc()
+	go atomicInc()
+	semaphore.Wait()
+	fmt.Println("Final atomicCounter value is:", atomicCounter)
 	fmt.Println()
 
 	elapsed := time.Since(start)
@@ -92,18 +101,42 @@ func decrement() {
 // Running go run -race main.go without the mutex will show this function has a
 // race condition as the counter variable is still being accessed at the same
 // time (potentially). However, this will still increment correctly as the
-// counter++ is a single instruction (atomic) and cannot context switch during
+// counter++ is a single instruction and cannot context switch during
 // its execution. Thus, counter will either not have incremented and it will
 // context switch, or counter will have incremented and then a context switch
 // can occur or not. Either way, regardless of the race condition the value of
 // counter is correct. This is not best practice however. Hence, we have used
-// a mutex here to avoid this race condition.
+// a mutex here to avoid this race condition. The increment() function above
+// shows how we can have a race condition within the function itself (by not
+// locking critical regions and allowing context switching between instructions)
+// as well as between different function calls (always due to context switching).
+// In this example, since there is only one instruction the same issue of critical
+// region doesn't apply. However, a mutex (lock) must still be used to avoid
+// thread switching (context switching) and causing a race condition with counter.
 func newInc() {
 	for i := 0; i < 10; i++ {
 		time.Sleep(time.Duration(rand.Intn(20))*time.Millisecond)
 		mutex.Lock()
 		counter++
 		mutex.Unlock()
+	}
+	semaphore.Done()
+}
+
+// Since this is a true atomic instruction (cannot be separated into smaller pieces)
+// this increment does not require a mutex to avoid the race condition.
+// Instead, we use atomic.AddInt64 which takes a pointer to the value we want
+// to add to, and a value to increase by. Here we provide 1 as we want to do ++.
+func atomicInc() {
+	for i := 0; i < 20; i++ {
+		time.Sleep(time.Duration(rand.Intn(20))*time.Millisecond)
+		atomic.AddInt64(&atomicCounter, 1)
+		// We sleep here to allow for context switch to show only the
+		// atomic instruction is thread safe, the prints etc. are not.
+		// If we do: go run -race main.go with these lines uncommented,
+		// we will get a race condition on atomicCounter due to the print.
+		//time.Sleep(time.Duration(rand.Intn(20))*time.Millisecond)
+		//fmt.Println(i, "atomicCounter:", atomicCounter)
 	}
 	semaphore.Done()
 }
