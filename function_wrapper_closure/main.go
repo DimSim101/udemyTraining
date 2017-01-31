be63ff636f49693f5c624e3b317a10c9910ac315
David Aaron <dsaaron123@gmail.com>
2017-01-31 16:10:00 +1100

    Added example to function wrapper example showing its use with concurrency and placing a critical region i.e. shared resource and its concurrency controllers i.e. mutex together to provide a wrapper with scope and encapsulation.

--- function_wrapper_closure/main.go
+++ function_wrapper_closure/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+	"math/rand"
+)
 
 // This is a function that initializes a count to 0 and returns a function.
 // This way we can init the variable and manipulate it with the returned function.
@@ -15,6 +20,22 @@ func wrapper() func() int {
 	}
 }
 
+func concurrencyWrapper() func() {
+	var counter int
+	var mutex sync.Mutex
+	return func () {
+		for i := 0; i < 10; i++ {
+			time.Sleep(time.Duration(rand.Intn(20))*time.Millisecond)
+			mutex.Lock()
+			counter++
+			fmt.Println("Counter value is:", counter)
+			mutex.Unlock()
+		}
+		semaphore.Done()
+	}
+}
+
+var semaphore sync.WaitGroup
 func main() {
 	// Here we get the function result that is returned from wrapper(),
 	// and calling wrapper() also initializes the int which increment() accesses.
@@ -23,6 +44,20 @@ func main() {
 	increment := wrapper()
 	fmt.Println(increment())
 	fmt.Println(increment())
+
+	// Here we can use this function wrapper() concept to initalize a mutex
+	// and a counter together which can then be manipulated using the
+	// function returned from concurrencyWrapper().
+	// We still use a global semaphore (WaitGroup) object so that main and
+	// concurrencyWrapper() both can access it within their scope.
+	// This is a nice example of how we can use functions, scope and closure
+	// to restrict variables to certain functions / combine variables together
+	// in a way that provides access and encapsulation with their scope.
+	concurrentInc := concurrencyWrapper()
+	semaphore.Add(2)
+	go concurrentInc()
+	go concurrentInc()
+	semaphore.Wait()
 }
 
 /*
