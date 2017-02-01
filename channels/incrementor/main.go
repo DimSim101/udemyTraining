5449d02c305bbd3796fee743361d0d4e95396696
David Aaron <dsaaron123@gmail.com>
2017-02-01 18:22:21 +1100

    Added example for incrementing a value. Extended example to use random numbers to show how we might actually use this to increment some value. If we knew how much we wanted to increment by ahead of time, it seems much easier to use a Mutex and WaitGroup (semaphore) and just manually control a single global variable using those concurrency primitives. However, being able to create a global counter int, specify some unknown number which becomes a channel containing that number of 1's, and then grab the total amount of those 1's means we can safely increment all the values behind the scenes (avoiding the need to lock and unlock) and then only increment the global variable once (utilizing the precalculated data). This means no lock / primitives are needed in this implementation. Whether it is worth while for clarity of code and/or issues like speed is another question.

---
+++ channels/incrementor/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"crypto/rand"
+)
+
+
+var counter int
+func main() {
+	counter = 42
+	fmt.Println("Counter =", counter)
+
+	incValues := randomSliceByte(2) // generate 2 random ints (a slice of
+	// random bytes of length 2).
+	fmt.Println(incValues)
+
+	fmt.Println("Incrementing counter by", incValues[0])
+	c1 := incrementByNumber(int(incValues[0]))
+	fmt.Println("Incrementing counter by", incValues[1])
+	c2 := incrementByNumber(int(incValues[1]))
+	c3 := newCount(c1)
+	c4 := newCount(c2)
+	newCountAddition := <-c3+<-c4
+	counter += newCountAddition
+	fmt.Println("Final Counter:", counter)
+}
+
+func incrementByNumber(number int) chan int {
+	out := make(chan int)
+	// Create a channel of ints containing a specified number of 1's
+	// so that we can increment our count by a value.
+
+	go func() {
+		for i := 0; i < number; i++ {
+			out <- 1
+		}
+		close(out)
+	}()
+	return out
+}
+
+func newCount(c chan int) chan int {
+	out := make(chan int)
+	go func() {
+		var newCount int
+		for n := range c {
+			newCount += n
+		}
+		out <- newCount
+		close(out)
+	}()
+	return out
+}
+
+func randomSliceByte(size int) []byte {
+	if size <= 0 {
+		return []byte{}
+	}
+
+	b := make([]byte, size)
+	_, error := rand.Read(b)
+	if error != nil {
+		fmt.Println(error)
+	}
+	return b
+}
