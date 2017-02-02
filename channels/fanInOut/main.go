64acc9eff4c5fc1a38399866a99c98cf8bfd0344
David Aaron <dsaaron123@gmail.com>
2017-02-02 18:13:43 +1100

    Added another fan in / fan out example showing how they work clearly and actually using fanout this time since the previous example didnt.

---
+++ channels/fanInOut/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+
+	var intSlice []uint64
+	for j := 0; j < 5; j++ {
+		for i := 0; i < 20; i++ {
+			intSlice = append(intSlice, uint64(i))
+		}
+	}
+
+	ints := gen(intSlice...) // create a channel of ints
+
+	// FAN OUT - PROCESS SINGLE CHANNEL IN MULTIPLE FUNCTIONS / GOROUTINES
+	values := factorial(ints) // start calculating factorial of ints in channel
+	values1 := factorial(ints) // start calculating factorial of ints in channel
+
+	// FAN IN - GRAB THE CHANNELS AND MERGE THE THEIR VALUES INTO ONE CHANNEL
+	for n := range merge(values, values1) {
+		fmt.Println(n)
+	}
+}
+
+func merge(chans ...<-chan uint64) <-chan uint64{
+	out := make(chan uint64)
+	var semaphore sync.WaitGroup
+	semaphore.Add(len(chans))
+
+	for _, channel := range chans {
+		go func(ch <-chan uint64) {
+			for n := range ch {
+				out <- n
+			}
+			semaphore.Done()
+		}(channel)
+	}
+
+	go func() {
+		semaphore.Wait()
+		close(out)
+	}()
+
+	return out
+}
+
+func gen(nums ...uint64) <-chan uint64{
+	out := make(chan uint64)
+	go func() {
+		for _, n := range nums {
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(in <-chan uint64)<-chan uint64 {
+	out := make(chan uint64)
+	go func() {
+		for n := range in {
+			out <- calculateFactorial(n)
+		}
+		close(out)
+	}()
+	return out
+}
+
+func calculateFactorial(num uint64) uint64 {
+	var factorialValue uint64 = 1
+	for i := num; i > 0; i-- {
+		factorialValue *= i
+	}
+	return factorialValue
+}
---
+++ channels/fan_in_and_out/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := fanIn(boring("David"), boring("Mandy"))
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-c)
+	}
+	fmt.Println("Your boring im peacing it...")
+}
+
+func boring(name string) <-chan string {
+	out := make(chan string)
+	go func() {
+		for i := 0; i < 5; i++ {
+			out <- fmt.Sprintf("%s - %d", name, i)
+		}
+	}()
+	return out
+}
+
+func fanIn(input1, input2 <- chan string) <- chan string {
+	out := make(chan string)
+	go func() {
+		for {
+			out <- <- input1
+		}
+	}()
+
+	go func() {
+		for {
+			out <- <- input2
+		}
+	}()
+	return out
+}
