96f83fb81e20df4b3672203946a14bdca9578b1c
David Aaron <dsaaron123@gmail.com>
2017-02-04 17:51:28 +1100

    Added another example with custom error handling. Also showed that custom errors are always strings as errors.New takes in a string.

--- error_handling/customErrors/main.go
+++ error_handling/customErrors/main.go
@@ -15,6 +15,20 @@ func main() {
 			fmt.Println("Factorial result is:", val)
 		}
 	}
+
+	myName, err := getName("BLAH")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(myName)
+	}
+
+	myName, err = getName("David")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(myName)
+	}
 }
 
 func factorial(num int) (int, error) {
@@ -27,4 +41,12 @@ func factorial(num int) (int, error) {
 		result *= i
 	}
 	return result, nil
+}
+
+func getName(name string) (string, error) {
+	if name == "David" {
+		// errors returned are always strings.
+		return "", errors.New("Hello me")
+	}
+	return "Your name is " + name, nil
 }
